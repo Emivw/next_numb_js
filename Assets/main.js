c078316871fc4c15dfa67d441c06c182e0c916db
Emivw <emiya.vanwyk@gmail.com>
2022-06-20 16:26:57 +0200

    sum

--- Assets/main.js
+++ Assets/main.js
@@ -1,14 +1,3 @@
-function rgbToHex() {
-    let inputVal = document.getElementById("rgbColor").value;
-    var values = /(\d{1,3})\D+(\d{1,3})\D+(\d{1,3})/g.exec(inputVal).splice(1, 3);
-
-    if (values.length !== 3) return false;
-
-    var hexValue = "#";
-    for (const value of values) {
-        hexValue += parseInt(value).toString(16);
-    }
-
-    document.getElementById("hexColor").value = hexValue;
-    document.body.style.backgroundColor = `${hexValue}`;
-}
+const array = [1, true, false, "dare", []];
+const count = array.filter(Boolean).length;
+console.log(count);
