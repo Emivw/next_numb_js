0cb4500706f19eb4d4862d5c504ee98e0adc445c
Emivw <emiya.vanwyk@gmail.com>
2022-06-20 11:49:53 +0200

    first

--- Assets/main.js
+++ Assets/main.js
@@ -1,11 +1,7 @@
 function calc() {
     let data = [5, 10, 50, 20, 15];
-    let sum = 0;
-
-    for (const value of data) {
-        sum += value;
-    }
-    document.getElementById('output').innerHTML = `the sum of array ${data} is ${sum}`;
+    let min = Math.min(...data);
+    let max = Math.max(...data);
+    document.getElementById('output').innerHTML = `the minimum value of the ${data} set is ${min} and the max is ${max}`;
     console.log(data);
-    console.log(sum);
 }
