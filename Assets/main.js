aec3cecc6863936a1d4daffcfd84a5dc8d782710
Emivw <emiya.vanwyk@gmail.com>
2022-06-20 12:05:12 +0200

    first

--- Assets/main.js
+++ Assets/main.js
@@ -1,7 +1,41 @@
-function calc() {
-    let data = [5, 10, 50, 20, 15];
-    let min = Math.min(...data);
-    let max = Math.max(...data);
-    document.getElementById('output').innerHTML = `the minimum value of the ${data} set is ${min} and the max is ${max}`;
-    console.log(data);
+const regex = /^#?([0-9a-fA-F]{3}){1,2}\b$/;
+let rColor, gColor, bColor, getHexColor, hexColor;
+
+function trueInput(val) {
+    let check = regex.test(val);
+    return check;
+}
+
+function hexToRGB() {
+    getHexColor = document.getElementById("hexColor").value;
+    if (trueInput(getHexColor)) {
+        hexColor = getHexColor.replace(/#/g, "");
+        if (hexColor.length === 6 || hexColor.length === 3) {
+            if (hexColor.length === 3) {
+                rColor = parseInt(hexColor.substring(0, 1).repeat(2), 16);
+                gColor = parseInt(hexColor.substring(1, 2).repeat(2), 16);
+                bColor = parseInt(hexColor.substring(2, 3).repeat(2), 16);
+                document.getElementById("rgbColor").value =
+                    "rgb(" + rColor + "," + gColor + "," + bColor + ")";
+                document.body.style.backgroundColor =
+                    "rgb(" + rColor + "," + gColor + "," + bColor + ")";
+            } else {
+                rColor = parseInt(hexColor.substring(0, 2), 16);
+                gColor = parseInt(hexColor.substring(2, 4), 16);
+                bColor = parseInt(hexColor.substring(4, 6), 16);
+                document.getElementById("rgbColor").value =
+                    "rgb(" + rColor + "," + gColor + "," + bColor + ")";
+                document.body.style.backgroundColor =
+                    "rgb(" + rColor + "," + gColor + "," + bColor + ")";
+            }
+        }
+    } else {
+        if (hexColor == "") {
+            document.getElementById("rgbColor").value = "Result in RGB format";
+        } else {
+            document.getElementById("rgbColor").value = "incorrect value";
+            document.getElementsByTagName("body")[0].style.backgroundColor =
+                "rgb(255,255,255)";
+        }
+    }
 }
