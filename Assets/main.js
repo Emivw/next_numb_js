fa67786738fdc8920f737879d8f51d4f99acd46d
Emivw <emiya.vanwyk@gmail.com>
2022-06-21 10:11:38 +0200

    color

--- Assets/main.js
+++ Assets/main.js
@@ -1,19 +1,3 @@
-// let data = [7, 8, 3, 'Peter', 98.8, true, 'Joel', false];
-// let onlyDigits = data.filter( (item)=> {
-//     return typeof item === 'number' ? item : null;
-// });
-// console.log(onlyDigits);
-// let onlyString = data.filter( (item)=> {
-//     return typeof item === 'string' ? item : null;
-// } );
-// console.log(onlyString);
-// let numbers = [2, 3, 4, 6];
-// ** = sqrt()
-// let modify = numbers.map( (item)=>{
-//     return item ** item;
-// } );
-// console.log(modify);
-// localStorage.removeItem('records');
 let lists = JSON.parse(localStorage.getItem("records")) ?
     JSON.parse(localStorage.getItem("records")) : [{
         name: "Emile",
