ad1a99a2f1a7bc3c12513b02b49a309618939eca
Emivw <emiya.vanwyk@gmail.com>
2022-06-21 11:30:20 +0200

    next

--- Assets/main.js
+++ Assets/main.js
@@ -1,40 +1,8 @@
-let lists = JSON.parse(localStorage.getItem("records")) ?
-    JSON.parse(localStorage.getItem("records")) : [{
-        name: "Emile",
-        surname: "vanwyk",
-        colorRgb: "333.222.3"
-    }];
-
-function addData() {
-    // e.preventDefault();
-    lists.push({
-        name: document.getElementById("name").value,
-        surname: document.getElementById("surname").value,
-        colorRgb: document.getElementById("rgbColor").value,
-    });
-    // Save data to a localstorage
-    localStorage.setItem("records", JSON.stringify(lists));
-}
-document.querySelector("#addRecord").addEventListener("click", addData);
-document.querySelector("#addRecord").addEventListener("click", rgbToHex);
-
-function rgbToHex() {
-    let inputVal = document.getElementById("rgbColor").value;
-    var values = /(\d{1,3})\D+(\d{1,3})\D+(\d{1,3})/g
-        .exec(inputVal)
-        .splice(1, 3);
-
-    if (values.length !== 3) return false;
-
-    var hexValue = "#";
-    for (const value of values) {
-        hexValue += parseInt(value).toString(16);
-    }
-
-    document.body.style.backgroundColor = `${hexValue}`;
-}
-// Displaying data
-(function loadData() {
-
-    console.table(lists);
-})();
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9];
+let iterator = numbers.values();
+let first = iterator.next().value;
+let second = iterator.next().value;
+let third = iterator.next().value;
+console.log(first);
+console.log(second);
+console.log(third);
